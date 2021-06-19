b5e690f333d53d4f205413c6bf86afcbf48abbfc
Sergei <ser90593696@yandex.ru>
2021-06-19 13:46:48 +0300

    test 2 prod v

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,4 @@
 'use strict'
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ?
-    '/table-contacts/' :
-    '/'
+  NODE_ENV: '"production"'
 }
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production' ?
+//         '/table-contacts/' :
+//         '/'
+// };
+
+
+// vue.config.js
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/table-contacts/' :
-        '/'
-};
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            publicPath: '/table-contacts/'
+            // изменение конфигурации для production...
+        } else {
+            // изменения для разработки...
+        }
+    }
+}
